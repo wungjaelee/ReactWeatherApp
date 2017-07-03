12c870062e7ea9f21df9ec771c6d369385ae4347
Wung Jae Lee <wunglee2019@u.northwestern.edu>
2017-07-04 01:16:20 +0900

    Style about page & link to resources

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -1,10 +1,28 @@
 var React = require('react');
+var {Link} = require('react-router');
 
 var About = (props) => {
   return (
     <div>
-      <h3>About</h3>
-      <p>Welcome to the about page</p>
+      <h1 className="text-center">About</h1>
+      <p>This is a weather application built on React.</p>
+      <p>
+        Here are some of the tools I used:
+      </p>
+      <ul>
+        <li>
+          <a href="https://facebook.github.io/react">React</a> - This was
+          the JavaScript frame used.
+        </li>
+        <li>
+          <a href="http://openWeathermap.org">Open Weather Map</a> - I used
+          Open Weather Map to search for weather data by city name.
+        </li>
+      </ul>
+      <p>Github Link:</p>
+      <Link to="https://github.com/wungjaelee/ReactWeatherApp">
+        https://github.com/wungjaelee/ReactWeatherApp
+      </Link>
     </div>
   )
 };
