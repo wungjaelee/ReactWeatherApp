c9cbd2b3f14cc24fc82a043fadb337b2a7d39c82
Wung Jae Lee <wunglee2019@u.northwestern.edu>
2017-07-04 15:15:48 +0900

    Added custom style to about, examples, and main page

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -4,7 +4,7 @@ var {Link} = require('react-router');
 var About = (props) => {
   return (
     <div>
-      <h1 className="text-center">About</h1>
+      <h1 className="text-center page-title">About</h1>
       <p>This is a weather application built on React.</p>
       <p>
         Here are some of the tools I used:
